34f6c0c80489fc1de8c7d67d62d1e5ef72fe8b56
Shane Harris <shane@xactaccounts.co.uk>
2019-06-20 19:46:41 +0100

    added option __package property to JSONApp interface!

--- desktop-app/src/app/repo-item/repo-item.component.ts
+++ desktop-app/src/app/repo-item/repo-item.component.ts
@@ -4,80 +4,77 @@ import { StatusBarService } from '../status-bar.service';
 import { AppService } from '../app.service';
 
 @Component({
-  selector: 'app-repo-item',
-  templateUrl: './repo-item.component.html',
-  styleUrls: ['./repo-item.component.css']
+    selector: 'app-repo-item',
+    templateUrl: './repo-item.component.html',
+    styleUrls: ['./repo-item.component.css'],
 })
 export class RepoItemComponent implements OnInit {
-  @Input('repo') repo:RepoItem;
-  @Input('i') i:number;
-  constructor(public repoService:RepoService,private appService:AppService,private statusService:StatusBarService) { }
+    @Input('repo') repo: RepoItem;
+    @Input('i') i: number;
+    constructor(public repoService: RepoService, private appService: AppService, private statusService: StatusBarService) {}
 
-  ngOnInit() {
-
-  }
-  deleteRepo(){
-    this.repoService.deleteRepo(this.i);
-    this.statusService.showStatus('Repo deleted OK!');
-  }
+    ngOnInit() {}
+    deleteRepo() {
+        this.repoService.deleteRepo(this.i);
+        this.statusService.showStatus('Repo deleted OK!');
+    }
 }
 
-export interface RepoItem{
-  name:string;
-  url:string;
-  icon:string;
-  order: number,
-  body: RepoBody,
-  categories:string[];
+export interface RepoItem {
+    name: string;
+    url: string;
+    icon: string;
+    order: number;
+    body: RepoBody;
+    categories: string[];
 }
-
 
 export interface JSONRepo {
-  timestamp:number;
-  version:number;
-  name:string;
-  icon:string;
-  address:string;
-  description:string;
+    timestamp: number;
+    version: number;
+    name: string;
+    icon: string;
+    address: string;
+    description: string;
 }
 
 export interface JSONApp {
-  antiFeatures:number[];
-  categories:string[];
-  suggestedVersionName:string;
-  suggestedVersionCode:string;
-  description:string;
-  issueTracker:string;
-  license:string;
-  name:string;
-  sourceCode:string;
-  summary:string;
-  webSite:string;
-  added:number;
-  icon:string;
-  packageName:string;
-  lastUpdated:number;
+    antiFeatures: number[];
+    categories: string[];
+    suggestedVersionName: string;
+    suggestedVersionCode: string;
+    description: string;
+    issueTracker: string;
+    license: string;
+    name: string;
+    sourceCode: string;
+    summary: string;
+    webSite: string;
+    added: number;
+    icon: string;
+    packageName: string;
+    lastUpdated: number;
+    __package?: any;
 }
 export interface JSONPackage {
-  added: number;
-  apkName: string;
-  hash: string;
-  hashType: string;
-  minSdkVersion: string;
-  nativecode: string[]
-  packageName: string;
-  sig: string;
-  size: number;
-  srcname: string;
-  targetSdkVersion: string;
-  versionCode: number;
-  versionName: string;
-  "uses-permission":any;
+    added: number;
+    apkName: string;
+    hash: string;
+    hashType: string;
+    minSdkVersion: string;
+    nativecode: string[];
+    packageName: string;
+    sig: string;
+    size: number;
+    srcname: string;
+    targetSdkVersion: string;
+    versionCode: number;
+    versionName: string;
+    'uses-permission': any;
 }
 export interface RepoBody {
-  repo:JSONRepo;
-  requests:{install:[0],uninstall:[0]};
-  apps:JSONApp[];
-  packages:any;
+    repo: JSONRepo;
+    requests: { install: [0]; uninstall: [0] };
+    apps: JSONApp[];
+    packages: any;
 }
-
