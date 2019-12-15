257cd9d388c2a5b018d441e8503efedf44411ee8
Shane Harris <shane@theexpanse.app>
2019-12-15 17:38:44 +0000

    added better scrcpy logging

--- desktop-app/src/app/app.service.ts
+++ desktop-app/src/app/app.service.ts
@@ -265,30 +265,28 @@ export class AppService {
 
     runScrCpy(options: any) {
         return new Promise((resolve, reject) => {
-            this.exec(
-                '' +
-                    this.scrcpyBinaryPath +
-                    ' --crop ' +
-                    options.crop +
-                    ' ' +
-                    ' -b ' +
-                    options.bit_rate +
-                    ' ' +
-                    (options.max_size ? ' --max-fps ' + options.max_fps + ' ' : '') +
-                    (options.max_size ? ' --max-size ' + options.max_size + ' ' : '') +
-                    (options.always_on_top ? '--always-on-top' : '') +
-                    ' ' +
-                    (options.fullscreen ? '-f' : '') +
-                    ' ' +
-                    (options.no_control ? '-n' : '') +
-                    ' --window-title "SideQuest Stream"',
-                function(err, stdout, stderr) {
-                    if (err) {
-                        return reject(err);
-                    }
-                    resolve(stdout);
+            let command =
+                this.scrcpyBinaryPath +
+                ' --crop ' +
+                options.crop +
+                ' ' +
+                ' -b ' +
+                options.bit_rate +
+                ' ' +
+                (options.max_size ? ' --max-fps ' + options.max_fps + ' ' : '') +
+                (options.max_size ? ' --max-size ' + options.max_size + ' ' : '') +
+                (options.always_on_top ? '--always-on-top' : '') +
+                ' ' +
+                (options.fullscreen ? '-f' : '') +
+                ' ' +
+                (options.no_control ? '-n' : '') +
+                ' --window-title "SideQuest Stream"';
+            this.exec(command, function(err, stdout, stderr) {
+                if (err) {
+                    return reject({ err, stderr, command });
                 }
-            );
+                resolve(stdout);
+            });
         });
     }
 
